23b306d2274b97c77c00a29468699751626b4870
Harleigh Awner <hawner@andrew.cmu.edu>
2016-11-13 11:09:03 -0500

    modified game element class to contain path to img global

--- Classes/Game_elements.py
+++ Classes/Game_elements.py
@@ -4,21 +4,25 @@
 
 from cocos.sprite import Sprite
 
+pathToImgs = '../Images/'
+
 class Wall(Object):
 
     def __init__(self):
-        self.overImg = 'Images/Overworld/o_wall.png'
-        self.underImg = 'Images/Upside_Down/u_wall.png'
+        global pathToImgs
+        self.overImg = pathToImgs + 'Overworld/o_wall.png'
+        self.underImg = pathToImgs + 'Upside_Down/u_wall.png'
         self.isMovable = False
         self.isSolid = True
 
 class Switch(Object):
 
     def __init__(self):
-        self.over_on_img = 'Images/Overworld/o_on-switch.png'
-        self.over_off_img = 'Images/Overworld/o_off-switch.png'
-        self.under_on_img = 'Images/Upside_Down/u_on-switch.png'
-        self.under_off_img = 'Images/Upside_Down/u_off-switch.png'
+        global pathToImgs
+        self.over_on_img = pathToImgs + 'Overworld/o_on-switch.png'
+        self.over_off_img = pathToImgs + 'Overworld/o_off-switch.png'
+        self.under_on_img = pathToImgs + 'Upside_Down/u_on-switch.png'
+        self.under_off_img = pathToImgs + 'Upside_Down/u_off-switch.png'
         self.isMovable = False
         self.isSolid = False
         self.isOn = False
@@ -26,44 +30,47 @@ class Switch(Object):
 class Rock(Object):
 
     def __init__(self):
-        self.overImg = 'Images/Overworld/o_stone.png'
-        self.underImg = 'Images/Upside_Down/u_stone.png'
+        global pathToImgs
+        self.overImg = pathToImgs + 'Overworld/o_stone.png'
+        self.underImg = pathToImgs + 'Upside_Down/u_stone.png'
         self.isMovable = True
         self.isSolid = True
 
 class Key(Object):
 
     def __init__(self):
-        self.img = 'Images/key.png'
+        global pathToImgs
+        self.img = pathToImgs + 'key.png'
 
 class Ladder(Object):
 
     def __init__(self):
-        self.overImg = 'Images/Overworld/o_ladder.png'
-        self.underImg = 'Images/Upside_Down/u_ladder.png'
+        global pathToImgs
+        self.overImg = pathToImgs + 'Overworld/o_ladder.png'
+        self.underImg = pathToImgs + 'Upside_Down/u_ladder.png'
         self.isMovable = False
         self.isSolid = False
 
 class Keywall(Object):
 
     def __init__(self):
-        self.overImg = 'Images/Overworld/o_keywall.png'
-        self.underImg = 'Images/Upside_Down/u_keywall.png'
+        self.overImg = pathToImgs + 'Overworld/o_keywall.png'
+        self.underImg = pathToImgs + 'Upside_Down/u_keywall.png'
         self.isMovable = False
         self.isSolid = True
 
 class Floor(Object):
 
     def __init__(self):
-        self.overImg = 'Images/Overworld/o_floor.png'
-        self.underImg = 'Images/Upside_Down/u_floor.png'
+        self.overImg = pathToImgs + 'Overworld/o_floor.png'
+        self.underImg = pathToImgs + 'Upside_Down/u_floor.png'
         self.isMovable = False
         self.isSolid = False
 
 class Portal(Object):
 
     def __init__(self):
-        self.img = 'Images/Portal.gif'
+        self.img = pathToImgs + 'Portal.gif'
         self.isMovable = False
         self.isSolid = False
 
